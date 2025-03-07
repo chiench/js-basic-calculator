6c7f979e33a4d98d1f9fa43baaefc57463644a8e
chienbk <quangminb53@gmail.com>
2025-03-07 22:58:15 +0700

    Refactor calculator UI and logic; enhance history display and input handling

--- assets/script.js
+++ assets/script.js
@@ -1,93 +1,89 @@
+const input = document.querySelector("#display");
 const btnAll = document.querySelectorAll(".btn");
 const btnEqual = document.querySelector("#calculate");
 const btnHistory = document.querySelector(".btn-history");
+const btnCalculator = document.querySelector(".btn-calculator");
 const container = document.querySelector(".container");
 const calculationScreen = document.querySelector(".calculation-screen");
+const historyScreen = document.querySelector(".history-screen");
+const ulHistoryList = document.querySelector(".history-list");
 
-const btnOperator = ["+", "-", "*", "/"];
+const btnOperator = ["+", "-", "*", "/", "%", "."];
 const btnNumber = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-
 const logCalculation = [];
-console.log("btnOperator: ", btnOperator);
-console.log("btnAll: ", btnAll);
-const input = document.querySelector("#display");
 
 let valueToFill = "";
 
 btnAll.forEach(function (btn) {
-  btn.addEventListener("click", function (event) {
-    console.log("event: ", event.target.innerHTML);
-    if (event.target.innerHTML === "=") {
-      console.log(2);
-    } else if (event.target.innerHTML === "CA") {
-      valueToFill = "";
-      input.value = "";
-    } else if (event.target.innerHTML === "DEL") {
-      console.log("valueToFill: ", valueToFill);
-      valueToFill = valueToFill.slice(0, -1);
-      input.value = valueToFill;
-    } else {
-      valueToFill = valueToFill + event.target.innerHTML;
-      console.log("valueToFill: ", valueToFill);
-      const lastCharacter = valueToFill.charAt(valueToFill.length - 1);
-      const lastSecondCharacter = valueToFill.charAt(valueToFill.length - 2);
-      console.log("lastCharacter: ", lastCharacter);
-      console.log("lastSecondCharacter: ", lastSecondCharacter);
-      if (
-        btnOperator.includes(event.target.innerHTML) &&
-        btnOperator.includes(lastCharacter) &&
-        btnOperator.includes(lastSecondCharacter)
-      ) {
-        valueToFill = valueToFill.slice(0, -2) + event.target.innerHTML;
-        input.value = valueToFill;
-      } else {
-        input.value = valueToFill;
-      }
-    }
-  });
+    btn.addEventListener("click", function (event) {
+        console.log("event: ", event.target.innerHTML);
+        if (event.target.innerHTML === "=") {
+            return;
+        } else if (event.target.innerHTML === "CA") {
+            valueToFill = "";
+            input.value = "";
+        } else if (event.target.innerHTML === "DEL") {
+            console.log("valueToFill: ", valueToFill);
+            valueToFill = valueToFill.slice(0, -1);
+            input.value = valueToFill;
+        } else {
+            valueToFill = valueToFill + event.target.innerHTML;
+            const lastCharacter = valueToFill.charAt(valueToFill.length - 1);
+            const lastSecondCharacter = valueToFill.charAt(
+                valueToFill.length - 2
+            );
+            if (
+                btnOperator.includes(event.target.innerHTML) &&
+                btnOperator.includes(lastCharacter) &&
+                btnOperator.includes(lastSecondCharacter)
+            ) {
+                valueToFill = valueToFill.slice(0, -2) + event.target.innerHTML;
+                input.value = valueToFill;
+            } else if (
+                btnOperator.includes(event.target.innerHTML) &&
+                valueToFill.length === 1
+            ) {
+                valueToFill = "";
+                input.value = "";
+            } else {
+                input.value = valueToFill;
+            }
+        }
+    });
 });
 btnEqual.addEventListener("click", function () {
-  console.log("valueToFill: ", valueToFill);
-  if (btnOperator.includes(valueToFill.charAt(valueToFill.length - 1))) {
-    console.warn("Please enter a valid expression");
-  } else {
-    logCalculation.push(valueToFill);
-    valueToFill = eval(valueToFill);
-    input.value = valueToFill;
-  }
+    console.log("valueToFill: ", valueToFill);
+    if (btnOperator.includes(valueToFill.charAt(valueToFill.length - 1))) {
+        console.warn("Please enter a valid expression");
+    } else if (valueToFill === "") {
+        console.warn("Please enter a valid expression");
+    } else {
+        logCalculation.push(valueToFill);
+        valueToFill = eval(valueToFill);
+        input.value = valueToFill;
+    }
 });
 btnHistory.addEventListener("click", function () {
-  calculationScreen.style.visibility = "hidden";
+    historyScreen.classList.remove("hidden");
+    calculationScreen.classList.add("hidden");
 
-  container.innerHTML = `
-  <div class="history-screen">  
-    <div class="title">
-                <h2>History</h2>
-                <button class="btn-calculator">
-                    <i class="fa-solid fa-calculator"></i>
-                    Máy tính</button>
-        </div>
-        <ul class="history-list">
-                
-        </ul>
-     </div>
-    `;
-  const btnCalculator = document.querySelector(".btn-calculator");
-  const historyScreen = document.querySelector(".history-screen");
-  btnCalculator.addEventListener("click", function () {
-    historyScreen.style.visibility = "hidden";
-    calculationScreen.style.visibility = "visible";
-    input.value = "";
+    if (logCalculation.length === 0) {
+        const li = document.createElement("li");
+        li.className = "history-item-empty";
+        li.innerHTML = "No history found";
+        ulHistoryList.appendChild(li);
+    }
+    logCalculation.forEach(function (log) {
+        const li = document.createElement("li");
+        li.className = "history-item";
+        li.innerHTML = `${log} = ${eval(log)}`;
+        ulHistoryList.appendChild(li);
+    });
+});
+btnCalculator.addEventListener("click", function () {
+    historyScreen.classList.add("hidden");
+    calculationScreen.classList.remove("hidden");
     valueToFill = "";
-  });
-  const ulEl = document.querySelector(".history-list");
-
-  logCalculation.forEach(function (calculation) {
-    let li = document.createElement("li");
-
-    li.className = "history-item";
-    // Tạo nội dung bên trong
-    li.innerHTML = `${calculation}`;
-    ulEl.appendChild(li);
-  });
+    input.value = "";
+    ulHistoryList.innerHTML = "";
 });
